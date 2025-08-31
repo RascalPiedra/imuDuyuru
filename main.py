24670cc2d06385ba2957066d963d50c2b336b242
RascalPiedra <120836710+RascalPiedra@users.noreply.github.com>
2025-08-31 21:39:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ conn = http.client.HTTPSConnection("api.pushover.net:443")
 # Son duyuruyu tutacak dosya
 file_path = "son_duyuru.txt"
 if os.path.exists(file_path):
-    with open(file_path, "rt", encoding="utf-8") as file:
+    with open(file_path, "rt") as file:
         x = file.read()
 else:
     x = ""
@@ -26,19 +26,17 @@ for i in first:
         duyuru = i.find("p")
         if a == 21 and duyuru:
             if x != duyuru.text:
-                with open(file_path, "w", encoding="utf-8") as file:
+                with open(file_path, "w") as file:
                     file.write(duyuru.text)
 
                 conn.request(
                     "POST",
                     "/1/messages.json",
                     urllib.parse.urlencode({
-                        "token": os.getenv("PUSHOVER_TOKEN"),
-                        "user": os.getenv("PUSHOVER_USER"),
+                        "token": "aseio3vnkrsyv6i2szjgen2efddyba",
+                        "user": "uiqzb6qk2e3yf81pbn5z8dr55od5wf",
                         "title": "Duyuru!!!",
                         "message": duyuru.text,
-                        "url": "",
-                        "priority": "0"
                     }),
                     {"Content-type": "application/x-www-form-urlencoded"}
                 )
