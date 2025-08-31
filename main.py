f10f874496b5246f34511b987ada298a8fbdcd81
RascalPiedra <120836710+RascalPiedra@users.noreply.github.com>
2025-09-01 01:22:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ else:
     x = ""
 
 a = 0
-print("Son kayitli duyuru:", x)
-
 for i in first:
     a += 1
     if 21 <= a <= 24:
@@ -29,9 +27,18 @@ for i in first:
         print(f"Index {a} →", duyuru.text if duyuru else "Bulunamadi")
         if a == 21 and duyuru:
             if x != duyuru.text:
-                print("Yeni duyuru bulundu → Pushover gönderiliyor...")
-                ...
-            else:
-                print("Duyuru değişmemiş, bildirim gönderilmiyor.")
+                with open(file_path, "w", encoding="utf-8") as file:
+                    file.write(duyuru.text)
 
-
+                conn.request(
+                    "POST",
+                    "/1/messages.json",
+                    urllib.parse.urlencode({
+                        "token": "aseio3vnkrsyv6i2szjgen2efddyba",
+                        "user": "uiqzb6qk2e3yf81pbn5z8dr55od5wf",
+                        "title": "Duyuru!!!",
+                        "message": duyuru.text,
+                    }),
+                    {"Content-type": "application/x-www-form-urlencoded"}
+                )
+                resp = conn.getresponse()
