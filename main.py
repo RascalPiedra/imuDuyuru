f361d9819c108ba61b066e1a6efda5d2b227d398
RascalPiedra <120836710+RascalPiedra@users.noreply.github.com>
2025-08-31 20:10:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,45 @@
-from time import sleep as delay
+import os
+import requests
 from bs4 import BeautifulSoup
 import http.client, urllib
-import requests
-
-
 
 url = "https://medeniyet.edu.tr/tr"
 
+req = requests.get(url).content
+soup = BeautifulSoup(req, "lxml")
 
+first = soup.find_all("div", class_="media-body")
+conn = http.client.HTTPSConnection("api.pushover.net:443")
 
-while True:
-    req =  requests.get(url).content
-    soup = BeautifulSoup(req, "lxml")
-
-    first = soup.find_all("div", class_="media-body")
-
-    conn = http.client.HTTPSConnection("api.pushover.net:443")
-
-
-    a = 0
-
-    try:
-        file = open("son_duyuru.txt", "rt")
+# Son duyuruyu tutacak dosya
+file_path = "son_duyuru.txt"
+if os.path.exists(file_path):
+    with open(file_path, "rt", encoding="utf-8") as file:
         x = file.read()
-        file.close()
-    except:
-        x = ""
+else:
+    x = ""
 
-    #print("-----------------------------------------DUYURULAR-----------------------------------------")
-    for i in first:
-        a += 1
-        if a >= 21 and a <= 24:
-            duyuru = i.find("p")
-            #print(duyuru.text)
-            if a == 21:
-                    if x == duyuru.text:
-                        #print("---Zaten duyuru bu.")
-                        with open("son_duyuru.txt", "w") as file:
-                            file.write(x)
-                    else:
-                        x = duyuru.text
-                        #print("---Yeni Duyuru!!!")
-                        with open("son_duyuru.txt", "w") as file:
-                            file.write(x)
-                        conn.request("POST", "/1/messages.json",
-                                     urllib.parse.urlencode({
-                                         "token":"aseio3vnkrsyv6i2szjgen2efddyba",
-                                         "user":"uiqzb6qk2e3yf81pbn5z8dr55od5wf",
-                                         "title":"Duyuru!!!",
-                                         "message": x,
-                                         "url":"",
-                                         "priority":"0"
-                                     }), {"Content-type":"application/x-www-form-urlencoded"})
-                        conn.getresponse()
+a = 0
+for i in first:
+    a += 1
+    if 21 <= a <= 24:
+        duyuru = i.find("p")
+        if a == 21 and duyuru:
+            if x != duyuru.text:
+                with open(file_path, "w", encoding="utf-8") as file:
+                    file.write(duyuru.text)
 
-
-
-
-    delay(900)
-    
+                conn.request(
+                    "POST",
+                    "/1/messages.json",
+                    urllib.parse.urlencode({
+                        "token": os.getenv("PUSHOVER_TOKEN"),
+                        "user": os.getenv("PUSHOVER_USER"),
+                        "title": "Duyuru!!!",
+                        "message": duyuru.text,
+                        "url": "",
+                        "priority": "0"
+                    }),
+                    {"Content-type": "application/x-www-form-urlencoded"}
+                )
+                conn.getresponse()
